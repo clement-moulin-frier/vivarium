7cdf35c92abcf4aa06713c96ee7f65b6da169ddd
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2023-04-01 16:08:15 +0200

    Update setup.py (test)

--- setup.py
+++ setup.py
@@ -1,8 +1,54 @@
-from setuptools import find_packages, setup
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import io
+import os
+import setuptools
 
-setup(
+# https://packaging.python.org/guides/making-a-pypi-friendly-readme/
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
+
+setuptools.setup(
     name='vivarium',
-    # extras_require=dict(tests=['pytest']),
-    packages=find_packages(where="src"),
-    package_dir={"": "src"}
-)
+    version='0.0.0',
+    license='MIT',
+    author='Clément Moulin-Frier',
+    author_email='clement.moulinfrier@gmail.com',
+    #install_requires=INSTALL_REQUIRES,
+    #url='https://github.com/google/jax-md',
+    packages=setuptools.find_packages(),
+    #download_url = "https://pypi.org/project/jax-md/",
+    #project_urls={
+    #    "Source Code": "https://github.com/google/jax-md",
+    #    "Documentation": "https://arxiv.org/abs/1912.04232",
+    #    "Bug Tracker": "https://github.com/google/jax-md/issues",
+    #},
+    #long_description=long_description,
+    #long_description_content_type='text/markdown',
+    #description='Differentiable, Hardware Accelerated, Molecular Dynamics',
+    #python_requires='>=2.7',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+    ])
