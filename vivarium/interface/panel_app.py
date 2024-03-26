3799d80fc670aad688c0889ad3fd21e44276eead
Marsolo1 <martialmarzloff@gmail.com>
2024-03-26 15:03:32 +0100

    fix negative value callback

--- vivarium/interface/panel_app.py
+++ vivarium/interface/panel_app.py
@@ -294,7 +294,7 @@ class WindowManager(Parameterized):
     def set_callbacks(self):
         # putting directly the slider value causes bugs on some OS
         self.pcb_plot = pn.state.add_periodic_callback(self.update_plot_cb,
-                                                       self.update_timestep.value or 0)
+                                                       abs(self.update_timestep.value or 0))
         self.entity_toggle.param.watch(self.entity_toggle_cb, "value")
         self.start_toggle.param.watch(self.start_toggle_cb, "value")
         self.update_switch.param.watch(self.update_switch_cb, "value")
