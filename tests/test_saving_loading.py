511f78301924ff1b9a4287ec6850a571f6ab408e
corentinlger <corentin.lger@gmail.com>
2024-03-27 20:00:56 +0100

    Update tests with new step function

--- tests/test_saving_loading.py
+++ tests/test_saving_loading.py
@@ -3,8 +3,8 @@
 
 import logging
 
-import jax.numpy as jnp
 import numpy as np
+import jax.numpy as jnp 
 
 from vivarium.simulator import behaviors
 from vivarium.simulator.sim_computation import dynamics_rigid, StateType
@@ -26,72 +26,70 @@ freq = 40.0
 neighbor_radius = 100.0
 to_jit = True
 use_fori_loop = False
-log_level = 'INFO'
 
 
-logging.basicConfig(level=log_level.upper())
-
-simulator_config = SimulatorConfig(
-    box_size=box_size,
-    n_agents=n_agents,
-    n_objects=n_objects,
-    num_steps_lax=num_steps_lax,
-    dt=dt,
-    freq=freq,
-    neighbor_radius=neighbor_radius,
-    to_jit=to_jit,
-    use_fori_loop=use_fori_loop
-)
-
-agent_configs = [
-    AgentConfig(idx=i,
-                x_position=np.random.rand() * simulator_config.box_size,
-                y_position=np.random.rand() * simulator_config.box_size,
-                orientation=np.random.rand() * 2. * np.pi)
-    for i in range(simulator_config.n_agents)
-    ]
-
-object_configs = [
-    ObjectConfig(idx=simulator_config.n_agents + i,
-                x_position=np.random.rand() * simulator_config.box_size,
-                y_position=np.random.rand() * simulator_config.box_size,
-                orientation=np.random.rand() * 2. * np.pi)
-    for i in range(simulator_config.n_objects)
-    ]
-
-state = converters.set_state_from_config_dict(
-    {
-        StateType.AGENT: agent_configs,
-        StateType.OBJECT: object_configs,
-        StateType.SIMULATOR: [simulator_config]
-    }
+def test_saving_loading():
+    simulator_config = SimulatorConfig(
+        box_size=box_size,
+        n_agents=n_agents,
+        n_objects=n_objects,
+        num_steps_lax=num_steps_lax,
+        dt=dt,
+        freq=freq,
+        neighbor_radius=neighbor_radius,
+        to_jit=to_jit,
+        use_fori_loop=use_fori_loop
     )
 
-num_steps = 10
-saving_name = "test_dir"
-nve_states = []
+    agent_configs = [
+        AgentConfig(idx=i,
+                    x_position=np.random.rand() * simulator_config.box_size,
+                    y_position=np.random.rand() * simulator_config.box_size,
+                    orientation=np.random.rand() * 2. * np.pi)
+        for i in range(simulator_config.n_agents)
+        ]
 
-simulator = Simulator(state, behaviors.behavior_bank, dynamics_rigid)
-assert not simulator.recording
+    object_configs = [
+        ObjectConfig(idx=simulator_config.n_agents + i,
+                    x_position=np.random.rand() * simulator_config.box_size,
+                    y_position=np.random.rand() * simulator_config.box_size,
+                    orientation=np.random.rand() * 2. * np.pi)
+        for i in range(simulator_config.n_objects)
+        ]
 
-lg.info("Running simulation")
-simulator.start_recording(saving_name)
-assert simulator.recording
+    state = converters.set_state_from_config_dict(
+        {
+            StateType.AGENT: agent_configs,
+            StateType.OBJECT: object_configs,
+            StateType.SIMULATOR: [simulator_config]
+        }
+        )
 
-# Run the simulation for num_steps and save the nve_state
-for _ in range(num_steps):
-    simulator.state, simulator.neighbors = simulator.step(simulator.state, simulator.neighbors)
-    nve_states.append(state.nve_state)
+    saving_name = "test_dir"
+    nve_states = []
 
-simulator.stop_recording()
-assert not simulator.recording
-assert not simulator.records
+    simulator = Simulator(state, behaviors.behavior_bank, dynamics_rigid)
+    assert not simulator.recording
 
-loaded_nve_states = simulator.load(saving_name)
-assert loaded_nve_states
+    lg.info("Running simulation")
+    simulator.start_recording(saving_name)
+    assert simulator.recording
 
-lg.info("Simulation complete")
+    # Run the simulation for num_steps and save the nve_state
+    for _ in range(num_steps):
+        simulator.step()
+        nve_states.append(simulator.state.nve_state)
 
-# At the momet the saving and the loading works but 
-for state, loaded_state in zip(nve_states, loaded_nve_states):
-    assert jnp.array_equal(state.position, loaded_state.position)
+    simulator.stop_recording()
+    assert not simulator.recording
+    assert not simulator.records
+
+    loaded_nve_states = simulator.load(saving_name)
+    assert loaded_nve_states
+
+    lg.info("Simulation complete")
+
+    # At the momet the saving and the loading works but there are numerical errors
+    for state, loaded_state in zip(nve_states, loaded_nve_states):
+        # This will therefore raise an error in the test :
+        assert jnp.array_equal(state.position, loaded_state.position)
