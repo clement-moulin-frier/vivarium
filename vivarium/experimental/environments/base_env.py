141625f670adf7da2c08f1d9f3be18a1ff167d02
corentinlger <corentin.lger@gmail.com>
2024-06-17 17:15:29 +0200

    Update base env by removing intermediate state classes

--- vivarium/experimental/environments/base_env.py
+++ vivarium/experimental/environments/base_env.py
@@ -1,5 +1,5 @@
 import logging as lg
-from enum import Enum
+
 from functools import partial
 from typing import Tuple
 
@@ -7,51 +7,13 @@ import jax.numpy as jnp
 
 from jax import  jit
 from flax import struct
-from jax_md import simulate
 
-
-# TODO : The best is surely to only define BaseState because some envs might not use EntityState / ObjectState or AgentState
-class EntityType(Enum):
-    AGENT = 0
-    OBJECT = 1
-
-# No need to define position, momentum, force, and mass (i.e already in jax_md.simulate.NVEState)
-@struct.dataclass
-class BaseEntityState(simulate.NVEState):
-    entity_type: jnp.array
-    entity_idx: jnp.array
-    diameter: jnp.array
-    friction: jnp.array
-    exists: jnp.array
-
-    @property
-    def velocity(self) -> jnp.array:
-        return self.momentum / self.mass
-    
-@struct.dataclass
-class BaseAgentState:
-    ent_idx: jnp.array
-    color: jnp.array
-
-@struct.dataclass
-class BaseObjectState:
-    ent_idx: jnp.array 
-    color: jnp.array
 
 @struct.dataclass
 class BaseState:
     time: jnp.int32
     box_size: jnp.int32
-    max_agents: jnp.int32
-    max_objects: jnp.int32
-    neighbor_radius: jnp.float32
-    dt: jnp.float32  # Give a more explicit name
-    collision_alpha: jnp.float32
-    collision_eps: jnp.float32
-    entities: BaseEntityState
-    agents: BaseAgentState
-    objects: BaseObjectState
-
+   
 
 class BaseEnv:
     def __init__(self):
