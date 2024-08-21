2f0938daf7b07dae7e462b5f81aad94a2b4611cc
corentinlger <corentin.lger@gmail.com>
2024-08-21 12:12:20 +0200

    Add env to simulator state convertions and test script

--- vivarium/simulator/new_simulator.py
+++ vivarium/simulator/new_simulator.py
@@ -15,25 +15,95 @@ import jax.numpy as jnp
 from jax import jit
 from jax import lax
 from jax_md import space, partition, dataclasses
+from flax import struct
 
 from vivarium.controllers import converters
-from vivarium.simulator.states import EntityType, SimulatorState
+from vivarium.simulator.states import EntityType, AgentState, EntityState, ObjectState, StateType
+# TODO :  # Import it later but atm defined in the same file
+# from vivarium.simulator.states import State as SimState
+from vivarium.environments.braitenberg.selective_sensing import State as EnvState
 
-lg = logging.getLogger(__name__)
 
 # TODO : Handle the simulator state, handle the weird jax_md functions ... 
 from jax_md.dataclasses import dataclass
+from jax_md import util, simulate, rigid_body
 
+lg = logging.getLogger(__name__)
 
-def env_to_sim_state(env_state):
-    sim_state = None 
-    return sim_state
-    
-def sim_to_env_state(sim_state):
-    env_state = None 
-    return env_state
+# TODO : just need to convert the simulator state and the elements inside en_state (not inside agents, objects ...)
+@dataclass
+class SimulatorState:
+    idx: util.Array
+    time: util.Array
+    # ent_sub_types: util.Array # TODO : Maybe add it later
+    box_size: util.Array
+    max_agents: util.Array
+    max_objects: util.Array
+    num_steps_lax: util.Array
+    dt: util.Array
+    freq: util.Array
+    neighbor_radius: util.Array
+    to_jit: util.Array
+    use_fori_loop: util.Array
+    collision_alpha: util.Array
+    collision_eps: util.Array
 
+    # DONE : Added time
+    @staticmethod
+    def get_type(attr):
+        if attr in ['idx', 'max_agents', 'max_objects', 'num_steps_lax']:
+            return int
+        elif attr in ['time', 'box_size', 'dt', 'freq', 'neighbor_radius', 'collision_alpha', 'collision_eps']:
+            return float
+        elif attr in ['to_jit', 'use_fori_loop']:
+            return bool
+        else:
+            raise ValueError(f"Unknown attribute {attr}")
+     
+@dataclass
+class SimState:
+    simulator_state: SimulatorState
+    entity_state: EntityState
+    agent_state: AgentState
+    object_state: ObjectState
 
+    def field(self, stype_or_nested_fields):
+        if isinstance(stype_or_nested_fields, StateType):
+            name = stype_or_nested_fields.name.lower()
+            nested_fields = (f'{name}_state', )
+        else:
+            nested_fields = stype_or_nested_fields
+
+        res = self
+        for f in nested_fields:
+            res = getattr(res, f)
+
+        return res
+
+    def ent_idx(self, etype, entity_idx):
+        return self.field(etype).ent_idx[entity_idx]
+
+    def e_idx(self, etype):
+        return self.entity_state.entity_idx[self.entity_state.entity_type == etype.value]
+
+    def e_cond(self, etype):
+        return self.entity_state.entity_type == etype.value
+
+    def row_idx(self, field, ent_idx):
+        return ent_idx if field == 'entity_state' else self.entity_state.entity_idx[jnp.array(ent_idx)]
+
+    def __getattr__(self, name):
+        def wrapper(e_type):
+            value = getattr(self.entity_state, name)
+            if isinstance(value, rigid_body.RigidBody):
+                return rigid_body.RigidBody(center=value.center[self.e_cond(e_type)],
+                                            orientation=value.orientation[self.e_cond(e_type)])
+            else:
+                return value[self.e_cond(e_type)]
+        return wrapper
+
+# TODO : Make that the state of simulator is the SimState (because will be used in CLient server communication)
+# TODO : Create a property method that returns env state as self.sim_to_env_state(self.state)
 class Simulator:
     def __init__(self, env, state, num_steps_lax=4, update_freq=-1, jit_step=True, use_fori_loop=True, seed=0):
         self.env = env
@@ -43,6 +113,7 @@ class Simulator:
         self.freq = update_freq
         self.jit_step = jit_step
         self.use_fori_loop = use_fori_loop
+        self.ent_sub_types = state.ent_sub_types # information about entities sub types in a dictionary, can't be given client side at the moment
 
         # transform the env state (only backend) into a jax md state with the older interface
         self.state = state
@@ -62,6 +133,54 @@ class Simulator:
         # TODO: Define which attributes are affected but these functions
         self.simulation_loop = self.select_simulation_loop_type(use_fori_loop)
 
+    # Done : Add a partial so if args of simulator change it will be changed in this fn
+    @partial(jax.jit, static_argnums=(0,))
+    def env_to_sim_state(self, env_state):
+        simulator_state = SimulatorState(
+            idx=jnp.array([2]),
+            time=jnp.array([env_state.time]),
+            box_size=jnp.array([env_state.box_size]),
+            max_agents=jnp.array([env_state.max_agents]),
+            max_objects=jnp.array([env_state.max_objects]),
+            dt=jnp.array([env_state.dt]),
+            neighbor_radius=jnp.array([env_state.neighbor_radius]),
+            collision_alpha=jnp.array([env_state.collision_alpha]),
+            collision_eps=jnp.array([env_state.collision_eps]),
+            num_steps_lax=jnp.array([self.num_steps_lax]),
+            freq=jnp.array([self.freq]),
+            use_fori_loop=jnp.array([self.use_fori_loop]),
+            to_jit=jnp.array([self.jit_step])
+        )
+
+        sim_state = SimState(
+            agent_state=env_state.agents,
+            entity_state=env_state.entities,
+            object_state=env_state.objects,
+            simulator_state = simulator_state
+        )
+
+        return sim_state
+        
+    @partial(jax.jit, static_argnums=(0,))
+    def sim_to_env_state(self, sim_state):
+        sim = sim_state.simulator_state
+
+        env_state = EnvState(
+            time=sim.time[0],
+            ent_sub_types=self.ent_sub_types,
+            box_size=sim.box_size[0],
+            max_agents=sim.max_agents[0],
+            max_objects=sim.max_objects[0],
+            dt=sim.dt[0],
+            neighbor_radius=sim.neighbor_radius[0],
+            collision_alpha=sim.collision_alpha[0],
+            collision_eps=sim.collision_eps[0],
+            entities=sim_state.entity_state,
+            agents=sim_state.agent_state,
+            objects=sim_state.object_state
+        )
+
+        return env_state
 
     # DONE : Remove num loops arg and removed 
     # TODO : Handle the num steps lax in environment side (not too hard to do)
@@ -81,10 +200,13 @@ class Simulator:
 
         return new_state
     
+
     def step(self):
         """Do a step in the simulation by calling _step"""
         self.state = self._step(self.state, self.num_steps_lax)
+        return self.state
 
+    # DONE : Added a return of the final state with run function
     def run(self, threaded=False, num_steps=math.inf, save=False, saving_name=None):
         """Run the simulator for the desired number of timesteps, either in a separate thread or not 
 
@@ -102,6 +224,8 @@ class Simulator:
             threading.Thread(target=_run).start()
         else:
             self._run(num_steps=num_steps, save=save, saving_name=saving_name)
+        return self.state
+
 
     def _run(self, num_steps, save, saving_name):
         """Function that runs the simulator for the desired number of steps. Used to be called either normally or in a thread.
@@ -140,6 +264,7 @@ class Simulator:
         self._is_started = False
         lg.info('Run stops')
 
+
     def update_sleep_time(self, frequency, elapsed_time):
         """Compute the time we need to sleep to respect the update frequency
 
@@ -168,6 +293,7 @@ class Simulator:
             state, neighbors = self.update_fn(i, (state, neighbors))
         return state, neighbors
 
+
     def lax_simulation_loop(self, state, neighbors, num_iterations):
         """Update the state and the neighbors on a few iterations with lax loop
 
@@ -177,6 +303,7 @@ class Simulator:
         """
         state, neighbors = lax.fori_loop(0, num_iterations, self.update_fn, (state, neighbors))
         return state, neighbors
+
 
     def select_simulation_loop_type(self, use_fori_loop):
         """Choose wether to use a lax or a classic simulation loop in function step
@@ -188,6 +315,7 @@ class Simulator:
         else:
             return self.classic_simulation_loop
         
+
     def start_recording(self, saving_name):
         """Start the recording of the simulation
         :param saving_name: optional name of the saving file
@@ -209,6 +337,7 @@ class Simulator:
         os.makedirs(self.saving_dir, exist_ok=True)
         lg.info('Saving directory %s created', self.saving_dir)
 
+
     def record(self, data):
         """Record the desired data during a step
         :param data: saved data (e.g simulator.state)
@@ -217,6 +346,7 @@ class Simulator:
             lg.warning('Recording not started yet.')
             return
         self.records.append(data)
+
 
     def save_records(self):
         """Save the recorded steps in a pickle file"""
@@ -229,6 +359,7 @@ class Simulator:
             pickle.dump(self.records, f)
             lg.info('Simulation frames saved in %s', saving_path)
 
+
     def stop_recording(self):
         """Stop the recording, save the recorded steps and reset recording information"""
         if not self.recording:
@@ -238,6 +369,7 @@ class Simulator:
         self.save_records()
         self.recording = False
         self.records = []
+
 
     def load(self, saving_name):
         """Load data corresponding to saving_name 
@@ -250,6 +382,7 @@ class Simulator:
             lg.info('Simulation loaded from %s', saving_path)
             return data
     
+
     # TODO : Add new attrbutes in this file when conversion functions are done
     def set_state(self, nested_field, ent_idx, column_idx, value):
         lg.info(f'set_state {nested_field} {ent_idx} {column_idx} {value}')
@@ -295,58 +428,15 @@ class Simulator:
         finally:
             self.run(threaded=True)
 
-
     # Other update functions
 
     def update_attr(self, attr, type_):
         lg.info('update_attr')
         setattr(self, attr, type_(getattr(self.state.simulator_state, attr)[0]))
 
-    # TODO : No more update space in the simulator
-    def update_space(self, box_size):
-        lg.info('update_space')
-        self.displacement, self.shift = space.periodic(box_size)
-
-    # TODO : No more update fn in the simulator
-    def update_function_update(self):
-        lg.info('update_function_update')
-        self.init_fn, self.step_fn = self.dynamics_fn(self.displacement, self.shift, self.behavior_bank)
-
-        def update_fn(_, state_and_neighbors):
-            state, neighs = state_and_neighbors
-            neighs = neighs.update(state.entity_state.position.center)
-            return (self.step_fn(state=state, neighbor=neighs, agent_neighs_idx=self.agent_neighs_idx),
-                    neighs)
-
-        if self.to_jit:
-            self.update_fn = jit(update_fn)
-        else:
-            self.update_fn = update_fn
-
-    # TODO : No more update fn in the simulator 
-    def init_state(self, state):
-        lg.info('init_state')
-        self.state = self.init_fn(state, self.key)
-
-    # Neighbor functions
-    # TODO : No more update neighbor functions in the simulator
-    def update_neighbor_fn(self, box_size, neighbor_radius):
-        lg.info('update_neighbor_fn')
-        self.neighbor_fn = partition.neighbor_list(self.displacement, box_size,
-                                                   r_cutoff=neighbor_radius,
-                                                   dr_threshold=10.,
-                                                   capacity_multiplier=1.5,
-                                                   # custom_mask_function=neigh_idx_mask,
-                                                   format=partition.Sparse)
-
-    def allocate_neighbors(self, position=None):
-        lg.info('allocate_neighbors')
-        position = self.state.entity_state.position.center if position is None else position
-        self.neighbors = self.neighbor_fn.allocate(position)
-        mask = self.state.entity_state.entity_type[self.neighbors.idx[0]] == EntityType.AGENT.value
-        self.agent_neighs_idx = self.neighbors.idx[:, mask]
-        return self.neighbors
-    
+    # DONE : No more update space in the simulator 
+    # DONE : No more update fn in the simulator 
+    # DONE : No more update neighbor functions in the simulator
 
     # Other functions
     # TODO : Remove ? 
@@ -356,3 +446,20 @@ class Simulator:
     def get_state(self):
         return self.state
     
+if __name__ == "__main__":
+    # Test for init, run and convertion functions
+    from vivarium.environments.braitenberg.selective_sensing import SelectiveSensorsEnv, init_state
+
+    env_state = init_state()
+    env = SelectiveSensorsEnv(state=env_state)
+
+    simulator = Simulator(env=env, state=env_state)
+
+    num_steps = 10
+    env_state = simulator.run(num_steps=num_steps)
+
+    sim_state = simulator.env_to_sim_state(env_state)
+    env_state = simulator.sim_to_env_state(sim_state)
+
+    env_state = simulator.run(num_steps=num_steps)
+    assert isinstance(env_state, EnvState)
