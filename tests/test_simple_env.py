325b61f68d5a6a24448813794c4c3624f3ea8581
corentinlger <corentin.lger@gmail.com>
2024-11-27 15:20:11 +0100

    Update simple braitenberg env test

--- tests/test_simple_env.py
+++ tests/test_simple_env.py
@@ -2,19 +2,8 @@ from vivarium.environments.braitenberg.simple.simple_env import init_state, Brai
 
 NUM_STEPS = 10
 
-def test_env_running_occlusion():
+def test_simple_env_running():
     """ Test the stepping mechanism of the env with occlusion (default) """
-    state = init_state()
-    env = BraitenbergEnv(state=state)
-
-    for _ in range(NUM_STEPS):
-        state = env.step(state=state)
-
-    assert env
-    assert state
-
-def test_env_running_no_occlusion():
-    """ Test the stepping mechanism of the env without occlusion """
     state = init_state()
     env = BraitenbergEnv(state=state)
 
