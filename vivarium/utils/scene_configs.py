34ede109eddc5e991502bd0808a8b7959bb8a4f9
corentinlger <corentin.lger@gmail.com>
2024-12-16 16:12:56 +0100

    Try to fix relative imports

--- vivarium/utils/scene_configs.py
+++ vivarium/utils/scene_configs.py
@@ -1,13 +1,15 @@
+import os 
 import logging
 
 import hydra
 
-from omegaconf import OmegaConf
 from omegaconf import OmegaConf, DictConfig
 from hydra import initialize, compose
 from hydra.core.global_hydra import GlobalHydra
 
-CONF_DIR_PATH = "../../conf"
+
+abs_config_dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../conf"))
+config_dir_path = os.path.relpath(abs_config_dir_path, start=os.path.dirname(__file__))
 
 
 def load_default_config() -> DictConfig:
@@ -15,7 +17,7 @@ def load_default_config() -> DictConfig:
 
     :return: default scene configuration
     """
-    with initialize(config_path=CONF_DIR_PATH, version_base=None):
+    with initialize(config_path=config_dir_path, version_base=None):
         cfg = compose(config_name="config")
         scene_config = OmegaConf.merge(cfg.default, cfg.scene)
     return scene_config
@@ -30,7 +32,7 @@ def load_scene_config(scene_name: str) -> DictConfig:
     if GlobalHydra().is_initialized():
         GlobalHydra().clear()
 
-    with hydra.initialize(config_path=CONF_DIR_PATH, version_base=None):
+    with hydra.initialize(config_path=config_dir_path, version_base=None):
         cfg = hydra.compose(config_name="config", overrides=[f"scene={scene_name}"])
         logging.basicConfig(level=cfg.log_level)
         scene_config = OmegaConf.merge(cfg.default, cfg.scene)
